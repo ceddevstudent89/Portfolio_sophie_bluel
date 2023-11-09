e66a60b57382ff922f03c9c3c2036e74abbea0d2
ceddevstudent89 <cedstudent89@gmail.com>
2023-11-09 11:51:56 +0100

    Partie style page login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,22 +1,3 @@
-let url = `http:localhost:5678/api/users/login`; // /user/login
-
-// Json
-/* 
-
-{
-  "email": "string",
-  "password": "string"
-}
-
-value
-status : 200 / application/json
-{
-  "userId": 1,
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4"
-}
-
-*/
-
 function login() {
   let email = document.getElementById("email").value;
   console.log(email);
@@ -41,6 +22,7 @@ function login() {
       }
     })
     .then((data) => {
+      console.log(data);
       localStorage.setItem("token", data.token);
       window.location.href = "../FrontEnd/index.html";
     })
