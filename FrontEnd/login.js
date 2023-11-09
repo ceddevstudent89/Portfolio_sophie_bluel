77f5425b71f69b0afcf94b3df31530d364a617a4
ceddevstudent89 <cedstudent89@gmail.com>
2023-11-09 09:30:43 +0100

    exercice

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,4 +1,56 @@
-const Form = document.getElementById("form");
-const connect = document.getElementById("connect");
-const email = document.getElementById("email");
-const password = document.getElementById("mdp");
+let url = `http:localhost:5678/api/users/login`; // /user/login
+
+// Json
+/* 
+
+{
+  "email": "string",
+  "password": "string"
+}
+
+value
+status : 200 / application/json
+{
+  "userId": 1,
+  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4"
+}
+
+*/
+
+function login() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("mdp").value;
+
+  let data = {
+    email: email,
+    password: password,
+  };
+
+  fetch("http:localhost:5678/api/users/login", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject("response.status");
+      }
+    })
+    .then((data) => {
+      localStorage.setItem("token", data.token);
+      window.location.href = "index.html";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// let form = document.getElementById("form");
+// console.log(form);
+
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  login();
+});
