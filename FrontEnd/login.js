63881fcd50ac021fa0637888c6a60d5ca0864d68
ceddevstudent89 <cedstudent89@gmail.com>
2023-11-16 01:05:29 +0100

    Vérification champs et ajout liens navigation

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,33 +1,88 @@
-function login() {
+const disconnected = document.querySelector(".disconnected");
+const emailError = document.querySelector(".email__error");
+const mdpError = document.querySelector(".mdp__error");
+
+// Si l'utilisateur est déjà connecté, on supprime le token
+
+loggingOutOfTheAdminPage();
+
+function loggingOutOfTheAdminPage() {
+  if (localStorage.getItem("token")) {
+    localStorage.removeItem("token");
+
+    const p = document.createElement("p");
+    p.innerHTML = "<br>Déconnexion. Veuillez saisir les champs ci-dessous";
+    p.setAttribute("style", "color: red");
+    disconnected.appendChild(p);
+    return;
+  }
+}
+
+function validateEmail(email) {
+  let emailRegExp = new RegExp(
+    "[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.[a-z0-9._-]+"
+  );
+  if (!emailRegExp.test(email)) {
+    const p = document.createElement("p");
+    p.innerHTML = "Veuillez entrer une addresse mail valide";
+    emailError.appendChild(p);
+    emailError.setAttribute("style", "color: red");
+    throw new Error("Erreur, Veuillez entrer une adresse mail valide");
+  }
+}
+
+function validateMdp(mdp) {
+  let emailRegExp = new RegExp("[a-zA-Z0-9]+");
+  if (!emailRegExp || mdp.length < 5) {
+    const p = document.createElement("p");
+    p.innerHTML = "Veuillez entrer un mot de passe valide";
+    mdpError.appendChild(p);
+    mdpError.setAttribute("style", "color: red");
+    throw new Error("Erreur, Veuillez entrer un mot de passe valide");
+  }
+}
+
+// Création de la fonction de connexion
+async function login() {
+  // récupération des valeurs du formulaire
   let email = document.getElementById("email").value;
-  console.log(email);
   let password = document.getElementById("mdp").value;
-  console.log(password);
+
+  validateEmail(email);
+  validateMdp(password);
 
   let data = {
     email: email,
     password: password,
   };
 
-  fetch("http://localhost:5678/api/users/login", {
+  // Appel de la fonction fetch qui contient les infos pour la connexion
+  await fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     body: JSON.stringify(data),
     headers: { "Content-Type": "application/json" },
   })
     .then((response) => {
+      // Vérification de la réponse
       if (response.ok) {
         return response.json();
       } else {
+        // Si erreur elle est rejetée et indique le status de l'erreur
         return Promise.reject(response.status);
       }
     })
     .then((data) => {
       console.log(data);
+      // stokage du token dans le local Storage
       localStorage.setItem("token", data.token);
-      window.location.href = "../FrontEnd/index.html";
+      // Redirection vers la page d'acceuil
+      if (localStorage.getItem("token")) {
+        window.location.href = "index.html";
+      }
     })
+    // Si erreur dans le code
     .catch((error) => {
-      console.log(error);
+      console.log(error.message);
     });
 }
 
--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -41,7 +41,7 @@ boutonfilterAll?.addEventListener("click", function () {
 const boutonfilterObject = document.querySelector(".filter-id-2");
 boutonfilterObject?.addEventListener("click", function () {
   const objectFilters = projets.filter(function (object) {
-    return object.category.name === "Objets";
+    return object.category.id === 1;
   });
   // Effacement de l'écran et regénération de la page
   document.querySelector(".gallery").innerHTML = "";
@@ -52,7 +52,7 @@ const boutonfilterApartment = document.querySelector(".filter-id-3");
 //console.log(boutonfilterApartment);
 boutonfilterApartment?.addEventListener("click", function () {
   const apartmentsFilters = projets.filter(function (apartment) {
-    return apartment.category.name === "Appartements";
+    return apartment.category.id === 2;
   });
   // Effacement de l'écran et regénération de la page
   document.querySelector(".gallery").innerHTML = "";
@@ -65,9 +65,21 @@ boutonfilterHotelsAndRestaurants?.addEventListener("click", function () {
   const hotelsAndRestaurantsFilters = projets.filter(function (
     hotelsAndRestaurants
   ) {
-    return hotelsAndRestaurants.category.name === "Hotels & restaurants";
+    return hotelsAndRestaurants.category.id === 3;
   });
   // Effacement de l'écran et regénération de la page
   document.querySelector(".gallery").innerHTML = "";
   genererProjets(hotelsAndRestaurantsFilters);
 });
+
+// Gérer la classe active des boutons
+const header = document.querySelector(".filters");
+const btns = header.getElementsByClassName("filter__btn");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    // console.log(current);
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+  });
+}
