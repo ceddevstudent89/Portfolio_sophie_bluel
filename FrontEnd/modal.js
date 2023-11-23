6ec3e7a58717011c5005edb6f8d7a359ac4b7054
ceddevstudent89 <cedstudent89@gmail.com>
2023-11-23 08:32:31 +0100

    modale partie fonctionnelle

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -9,28 +9,28 @@ const openModalBtn = document.querySelector(".btn-open");
 const closeModalBtn = document.querySelector(".btn-close");
 //
 const openModal = function () {
-  modal?.classList.remove("hidden");
-  overlay?.classList.remove("hidden");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 //
 const closeModal = function () {
-  modal?.classList.add("hidden");
-  overlay?.classList.add("hidden");
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 //
-openModalBtn?.addEventListener("click", openModal);
-closeModalBtn?.addEventListener("click", closeModal);
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
 //
 overlay?.addEventListener("click", closeModal);
 //
 document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape" && !modal?.classList.contains("hidden")) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
 
 document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape" && !modal2?.classList.contains("hidden")) {
+  if (event.key === "Escape" && !modal2.classList.contains("hidden")) {
     closeModal3();
   }
 });
@@ -42,30 +42,30 @@ const closeModalBtn3 = document.querySelector(".btn-close3");
 const overlay2 = document.querySelector(".overlay2");
 
 const openModal2 = function () {
-  modal?.classList.add("hidden");
-  overlay?.classList.add("hidden");
-  modal2?.classList.remove("hidden");
-  overlay2?.classList.remove("hidden");
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  modal2.classList.remove("hidden");
+  overlay2.classList.remove("hidden");
 };
 
 const closeModal2 = function () {
-  modal?.classList.remove("hidden");
-  overlay?.classList.remove("hidden");
-  modal2?.classList.add("hidden");
-  overlay2?.classList.add("hidden");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  modal2.classList.add("hidden");
+  overlay2.classList.add("hidden");
 };
 
 const closeModal3 = function () {
-  modal?.classList.add("hidden");
-  overlay?.classList.add("hidden");
-  modal2?.classList.add("hidden");
-  overlay2?.classList.add("hidden");
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  modal2.classList.add("hidden");
+  overlay2.classList.add("hidden");
 };
 
-openModalBtn2?.addEventListener("click", openModal2);
-closeModalBtn2?.addEventListener("click", closeModal2);
-closeModalBtn3?.addEventListener("click", closeModal3);
-overlay2?.addEventListener("click", closeModal3);
+openModalBtn2.addEventListener("click", openModal2);
+closeModalBtn2.addEventListener("click", closeModal2);
+closeModalBtn3.addEventListener("click", closeModal3);
+overlay2.addEventListener("click", closeModal3);
 //
 function genererProjets(projets) {
   for (let i = 0; i < projets.length; i++) {
